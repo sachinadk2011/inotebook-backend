e0ac698857c0c50d0b3a320df427a508ffc73f52
sachinadk2011 <adhikarisachin502@gmail.com>
2024-07-10 21:38:04 +0545

    initial code of backend

---
+++ db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const mongoURL = "mongodb://localhost:27017/";
+
+const connnectToMongo = async ()=>{
+    try {
+        await mongoose.connect(mongoURL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to MongoDB successfully');
+    } catch (err) {
+        console.error('Connection Error:', err);
+    }
+}
+
+module.exports = connnectToMongo;
---
+++ index.js
@@ -0,0 +1,15 @@
+const conectToMonngo = require('./db');
+const express = require('express')
+
+conectToMonngo();
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
