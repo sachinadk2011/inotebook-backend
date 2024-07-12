32e714ae998f111ad8209684ece9053b24261be3
sachinadk2011 <adhikarisachin502@gmail.com>
2024-07-12 19:40:28 +0545

    adding new folder module and rouute

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const app = express()
 const port = 5000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello sachin')
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
