e9e3070a4acf157271fd7c06b1a70388aeb3da36
Sachin adk <168266874+sachinadk2011@users.noreply.github.com>
2025-06-05 17:52:34 +0545

    small change in index.js

--- index.js
+++ index.js
@@ -20,13 +20,6 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
-// Serve static files from the frontend
-app.use(express.static(path.join(__dirname, "client/build"))); // Adjust path if needed
-
-// Catch-all route to serve React index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
-});
 
 
 app.listen(port, () => {
